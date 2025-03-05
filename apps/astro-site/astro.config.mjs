020b8236a4c2c9048ddb5810146c93091d1a38cc
Hayato Sakaguchi <hayato8190+univ@gmail.com>
2025-03-05 17:27:25 +0900

    quick fix

--- apps/astro-site/astro.config.mjs
+++ apps/astro-site/astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-  // GitHub Pagesのベースパス設定
-  base: '/slides',
-  build: {
-    // 静的ファイルの出力先
-    outDir: './dist',
-  }
+	integrations: [mdx()],
+	// GitHub Pagesのベースパス設定
+	base: '/slides',
+	build: {
+		// 静的ファイルの出力先
+		outDir: './dist',
+		assets: 'assets',
+	},
 });
